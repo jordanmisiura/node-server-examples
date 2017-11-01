29ba19a71febc4f14f4a50df0975284a0413a7e5
jordan misiura <jordanmisiura@gmail.com>
2017-11-01 13:41:24 -0700

    packages.json  and POST example
    npm init  as well as our first POST example  (no data saved)

--- nodeserverwork/server.js
+++ nodeserverwork/server.js
@@ -1,12 +1,3 @@
-//non express server
-// var http = require("http");
-
-// function process_request(req, res) {
-// 	res.end("f you world");
-// }
-
-// var s = http.createServer(process_request);
-// s.listen(8080);
 
 const express = require('express');
 const request = require('request-promise-native');
@@ -15,10 +6,13 @@ const cors = require('cors');
 const app = express();
 const pt = require('./promisetest');
 const rxt = require('./rxtest');
+const bodyParser = require('body-parser');
 
 //*****  turning on cors to allow cross domain  *****
 app.use(cors());
 //***************************************************
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/sharedid/:sharedid', function(req, res) { //13810705
 	const uri = 'https://www.cspire.tv/guide/v5.1/lineup/cspire/paytv/1.0/shared_ref/details.json?shared_ref_ids=tms:'+req.params.sharedid;
@@ -32,22 +26,23 @@ app.get('/sharedid/:sharedid', function(req, res) { //13810705
 		});
 });
 
-app.get('/logger.js', function(req, res) {
-	console.log("message from logger: " );
-	console.log(res);
-	res.end('message from logger: ', res);
+app.get('/:flavor', function(req, res) {
+
+	//pt.test1();
+	rxt.rxTest1();
+
+
+	res.end(' flavor is: ' + req.params.flavor);
 });
 
-// app.get('/:flavor', function(req, res) {
-
-// 	//pt.test1();
-// 	rxt.rxTest1();
-
-
-// 	res.end(' flavor is: ' + req.params.flavor);
-// });
-
 app.get('*', fourohfour);
+
+app.post('/password', (req, res) => {
+		console.log("post command activated from password url: ");
+		console.log(req.body);
+
+		res.end('received password');
+});
 
 function fourohfour(req, res) {
 	handleError(res, 404, "invalid file");
@@ -58,4 +53,6 @@ function handleError(res, errcode, message) {
 	res.end(JSON.stringify({ error: errcode, message: message}) + "\n");
 }
 
-app.listen(8080);
+app.listen(8080, function() {
+	console.log("server started on port 8080.");
+});
--- react/my-app/src/js/PingServerButton.js
+++ react/my-app/src/js/PingServerButton.js
@@ -5,18 +5,18 @@ class PingServerButton extends React.Component {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState === 4 && this.status === 200) {
-       
+        console.log("xhttp request object finished.");
       }
     };
 
-    try {
-      xhttp = "";
-      xhttp.open("GET", "http://localhost:8080/logger.js", true);
-      //xhttp.open("GET", "http://localhost:8080/cspire", true);
-      xhttp.send();
-    } catch(e) {
-      throw new Error('blah');
-    }
+    console.log("pushing button");
+
+    //xhttp.open("GET", "http://localhost:8080/cspire", true);
+    //xhttp.send();
+
+    xhttp.open("POST", 'http://localhost:8080/password', true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("password=myPassword&somevariable=poop");
   }
 
   render() {
